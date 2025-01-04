7c99aedf8052c1be14a67cf0e6b6f18544576e70
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-01-04 06:12:52 +0200

    dashboard assets

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [DashboardController::class, 'index']);
