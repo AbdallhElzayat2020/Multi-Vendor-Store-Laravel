5b8aaaf972beae7b66f031233327858d38d53d06
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-01-04 13:27:24 +0200

    Model Configuration

---
+++ app/Models/Store.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    // IF I want to change the created_at and updated_at
+    // const CREATED_AT = 'created_on';
+    // const UPDATED_AT = 'updated_on';
+
+    // if I Have a multi connection database
+    // protected $connection = 'mysql';
+
+    // if I want to change the primary key
+    // protected $primaryKey = 'id';
+
+    // if I want to change the auto incrementing
+    // public $incrementing = false , true;
+
+    // if I want to change the timestamps
+    // protected $timestamps = true , false;
+
+
+    protected $table = 'stores';
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'logo_image',
+        'cover_image',
+        'status',
+    ];
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        // new method
+        // $user = new User();
+        // $user->name = "Admin";
+        // $user->email = "admin@gmail.com";
+        // $user->password = Hash::make('password'); //  password
+        // $user->phone_number = rand(123456789, 9999999999);
+        // $user->save();
+
+
+        // create method
+        User::create([
+            'name' => "user",
+            'email' => "user@gmail.com",
+            'password' => Hash::make('password'), //  password
+            'phone_number' => '0123456788',
+        ]);
+
+        // query builder
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => "admin@gmail.com",
+            'password' => Hash::make('password'), //  password
+            'phone_number' => '0123456789',
+        ]);
+    }
+}
