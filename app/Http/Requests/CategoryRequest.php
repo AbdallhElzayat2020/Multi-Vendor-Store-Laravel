b2c4488140d2b8305b587bcfabc12fdeeb0d2707
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-01-08 22:02:39 +0200

    edits for requests Validation

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -30,12 +30,12 @@ class CategoryRequest extends FormRequest
             // 'image' => 'nullable|image|mimes:jpeg,png,jpg|max:1048576|mimetypes:mimetypes:image/jpeg,image/png,image/gif',
 
 
-
             'name' => 'required|string|min:3|max:255|unique:categories,name,' . $this->route('category'),
             'description' => 'nullable|string|max:500',
             'status' => 'required|in:archived,active',
             'parent_id' => 'nullable|integer|exists:categories,id',
             'image' => 'nullable|image|mimes:jpeg,png,jpg|max:1048576',
+
             // dimensions:min_width=100,min_height=100
         ];
     }
