39cce09f43e205eb9278e513768dff9d962a6475
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-01-10 13:51:51 +0200

    routes clean code

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -19,11 +19,14 @@ Route::group([
 
 
     // soft delete routes
-    Route::get('categories/trash', [CategoryController::class, 'trash'])->name('categories.trash');
+    Route::get('categories/trash', [CategoryController::class, 'trash'])
+        ->name('categories.trash');
 
-    Route::put('categories/{category}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+    Route::put('categories/{category}/restore', [CategoryController::class, 'restore'])
+        ->name('categories.restore');
 
-    Route::delete('categories/{category}/delete', [CategoryController::class, 'forceDelete'])->name('categories.force-delete');
+    Route::delete('categories/{category}/delete', [CategoryController::class, 'forceDelete'])
+        ->name('categories.force-delete');
 
     Route::resource('categories', CategoryController::class);
 
