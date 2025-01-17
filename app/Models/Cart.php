bdc85869354693c8d61912dd3f8327ee4b4e6f87
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-01-17 08:38:48 +0200

    Cart Observer

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use App\Observers\CartObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Cart extends Model
 {
+
     use HasFactory;
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'user_id',
+        'cookie_id',
+        'product_id',
+        'quantity',
+        'options',
+    ];
+
+    // observer
+    public static function booted()
+    {
+        static::observe(CartObserver::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id')->withDefault([
+            'name' => 'Guest',
+        ]);
+    }
 }
