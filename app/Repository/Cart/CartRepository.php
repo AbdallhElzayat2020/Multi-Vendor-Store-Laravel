8ada778f9035217c9fea7ff81e9fcdffcbf64cbb
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-01-24 17:50:47 +0200

    Add to Cart Complete full Code Fullstack

--- app/Repository/Cart/CartRepository.php
+++ app/Repository/Cart/CartRepository.php
@@ -3,84 +3,38 @@
 namespace App\Repository\Cart;
 
 use App\Interfaces\Cart\CartRepositoryInterface;
-use App\Models\Cart;
 use App\Models\Product;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Cookie;
-use Illuminate\Support\Str;
 
 class CartRepository implements CartRepositoryInterface
 {
 
-    // create Cookie
-    public function getCookieId()
-    {
-        $cookieId = Cookie::get('cookie_id');
-
-        if (!$cookieId) {
-            $cookieId = Str::uuid();
-            Cookie::queue('cookie_id', $cookieId, 60 * 24 * 30);
-        };
-
-        return $cookieId;
-    }
-    // get Cart
     public function get()
     {
-        $items = Cart::with('product')->where('cookie_id', '=', $this->getCookieId())->get();
-
-        return view('front.cart.index', compact('items'));
+        // TODO: Implement get() method.
     }
 
-    // add to Cart
     public function add(Product $product, $quantity = 1)
     {
-
-        $item = Cart::where('cookie_id', '=', $this->getCookieId())
-            ->where('product_id', '=', $product->id)
-            ->first();
-
-        if (!$item) {
-            Cart::create([
-                'cookie_id' => $this->getCookieId(),
-                'user_id' => auth()->id(),
-                'product_id' => $product->id,
-                'quantity' => $quantity,
-            ]);
-        }
-
-        $item->increment('quantity', $quantity);
-
-        return redirect()->route('cart.index');
+        // TODO: Implement add() method.
     }
 
-    // update Cart
     public function update(Product $product, $quantity)
     {
-        Cart::where('product_id', '=', $product->id)
-            ->where('cookie_id', '=', $this->getCookieId())
-            ->update([
-                'quantity' => $quantity,
-            ]);
+        // TODO: Implement update() method.
     }
 
-    // delete Cart
     public function delete($id)
     {
-        Cart::where('id', $id)
-            ->where('cookie_id', '=', $this->getCookieId())
-            ->delete();
+        // TODO: Implement delete() method.
     }
 
-    // empty Cart
     public function empty()
     {
-        Cart::where('cookie_id', '=', $this->getCookieId())->delete();
+        // TODO: Implement empty() method.
     }
 
-    // total Cart
     public function total()
     {
-        return Cart::where('cookie_id', '=', $this->getCookieId())->sum('quantity');
+        // TODO: Implement total() method.
     }
 }
